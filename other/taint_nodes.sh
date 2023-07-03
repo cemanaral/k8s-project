ce22cf077ebbad5b531f0a534b4a0c82eae46ea1
Cem Anaral <cemanaral@hotmail.com>
2023-07-03 23:55:13 +0300

    add taints

---
+++ other/taint_nodes.sh
@@ -0,0 +1,3 @@
+kubectl taint nodes worker-1 environment=production:NoExecute
+kubectl taint nodes worker-2 environment=production:NoExecute
+kubectl taint nodes worker-3 environment=production:NoExecute
